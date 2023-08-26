9c200472023cdc9cf9ae20303cc482ebecbc24fe
Ruan Chagas <ruanchagas95@gmail.com>
2023-08-26 00:43:31 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      hello world
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import GearIcon from '../assets/GearIcon'
+import { useState, useRef, useEffect } from 'react'
+import { getCategoriesKeys } from '../utils/getData'
+import styled from 'styled-components'
+
+const CategoryStyle =  styled.li`
+        display: inline-block;
+        margin: 0.25em;
+        padding: 0.25em 1em;
+        border: 2px solid rgba(255, 255, 255, 0.2);
+        border-radius: 4px;
+        color: #ffffff;
+    `
+const CategoriesStyle = styled.ul`
+
+`
+const HeaderStyle = styled.header`
+  position: fixed;
+  display: flex;
+  right: 10px;
+  top: 10px;
+  align-items: center;
+  height: 44px;
+`
+export default function Header() {
+  const [configIsVisible, setConfigIsVisible] = useState(false)
+  const listEl = useRef(null)
+  useEffect(() => {
+    console.log(listEl)
+  })
+  return (
+    <HeaderStyle>
+      {configIsVisible && (
+        <CategoriesStyle ref={listEl}>
+          {
+            getCategoriesKeys()
+              .map((category) => <CategoryStyle key={category}>{category}</CategoryStyle>)
+          }
+        </CategoriesStyle>
+      )}
+      <GearIcon width="32" height="32" onClick={() => setConfigIsVisible((prevState) => !prevState)} color='#ffffff6f' />
+    </HeaderStyle>
+  )
+}
